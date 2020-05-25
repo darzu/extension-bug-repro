8deb6a2be5cb27a015854bcea8f4b491d7ff8885
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2020-05-24 17:42:27 -0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,10 @@
+namespace foobar {
+    //% block="foo $a $b || $c $d"
+    export function foo(
+        a: number,
+        b: number,
+        c: number,
+        d: number): number {
+        return  0
+    }
+}
